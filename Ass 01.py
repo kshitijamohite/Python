04f8430eb0c87238d00c426faad2c9e5e6f327fb
Kshitija Mohite <68637387+kshitijamohite@users.noreply.github.com>
2021-03-22 22:27:36 +0530

    Add files via upload

---
+++ Ass 01.py
@@ -0,0 +1,16 @@
+x =20
+y =10
+
+print('Addition is :',x+y)
+
+print('Subtraction is :',x-y)
+
+print('Multiplication is :',x*y)
+
+print('Division is :',x/y)
+
+print('Integer division is :',x//y)
+
+print('Modulus is :',x%y)
+
+print('Power is :',x**y)
